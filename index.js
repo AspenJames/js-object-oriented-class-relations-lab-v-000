5c51cb5551d7fb6ab362a5fd0b6b7b355008bb5d
AspenJames <aspenjames@tqca.org>
2018-07-06 17:13:22 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,75 @@
+let store = {drivers: [], trips: [], passengers: []};
+
+class Driver{
+  constructor(name){
+    this.id = ++driverId;
+    this.name = name;
+
+    store.drivers.push(this);
+  };
+
+  trips(){
+    return store.trips.filter(function(trip){
+      return trip.driverId === this.id;
+    }.bind(this));
+  };
+
+  passengers(){
+    const passengers = [];
+    this.trips().forEach(function(trip){
+      passengers.push(trip.passenger());
+    });
+    return passengers;
+  };
+
+};
+let driverId = 0;
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerId;
+    this.name = name;
+
+    store.passengers.push(this);
+  };
+
+  trips(){
+    return store.trips.filter(function(trip){
+      return trip.passengerId === this.id
+    }.bind(this));
+  };
+
+  drivers(){
+    const drivers = [];
+    this.trips().forEach(function(trip){
+      drivers.push(trip.driver());
+    });
+    return drivers;
+  };
+
+};
+let passengerId = 0;
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+
+    store.trips.push(this);
+  };
+
+  passenger(){
+    return store.passengers.find(function(passenger){
+      return passenger.id === this.passengerId;
+    }.bind(this));
+  };
+
+  driver(){
+    return store.drivers.find(function(driver){
+      return driver.id === this.driverId;
+    }.bind(this));
+  };
+
+};
+let tripId = 0;
